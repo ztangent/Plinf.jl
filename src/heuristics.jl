f013d564a9f02a2f1e4b918462fc90c099a75797
Xuan <tanqazx@gmail.com>
2020-03-03 11:39:36 -0500

    Rewrite heuristics to use new indexing syntax for states.

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -1,18 +1,14 @@
 "Heuristic that counts the number of goals unsatisfied in the domain."
 function goal_count(goals, state, domain)
-    count = 0
-    for g in goals
-        sat, _ = satisfy(g, state, domain)
-        count += sat ? 0 : 1
-    end
+    count = sum([state[domain, g] for g in goals])
     return count
 end
 
 "Manhattan distance heuristic."
 function manhattan(goals, state, domain; fluents=@julog([xpos, ypos]))
     goal = PDDL.clauses_to_state(Vector{Clause}(goals))
-    goal_vals = [evaluate(f, goal, domain) for f in fluents]
-    curr_vals = [evaluate(f, state, domain) for f in fluents]
-    dist = sum(abs(g.name - c.name) for (g, c) in zip(goal_vals, curr_vals))
+    goal_vals = [goal[domain, f] for f in fluents]
+    curr_vals = [state[domain, f] for f in fluents]
+    dist = sum(abs.(goal_vals - curr_vals))
     return dist
 end
