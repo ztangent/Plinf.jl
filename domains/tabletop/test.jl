39e2ee0604f1794f57ffa2ba76ae4740635b472e
Jordyn Mann <jordynm@mit.edu>
2020-04-22 10:41:11 -0400

    Finish/fix PDDL->scene graph conversion code, add new blocksworld problem and corresponding test, fixes to domain

--- domains/tabletop/test.jl
+++ domains/tabletop/test.jl
@@ -2,8 +2,9 @@ using Test
 using InverseTAMP
 using Julog
 using PDDL
+include(joinpath(dirname(pathof(InverseTAMP)), "scene-graph-utils.jl"))
 
-function test()
+function test1()
     path = joinpath(dirname(pathof(InverseTAMP)), "..", "domains", "tabletop")
     @info "Path is $path"
 
@@ -21,7 +22,67 @@ function test()
     @test satisfy(@julog(handempty), state, domain)[1] == true
     @test satisfy(@julog(amounton(block1) == 0), state, domain)[1] == true
     @test available(@julog(pickup(table, block1)), state, domain)[1] == true
+
+    save_path = joinpath(dirname(pathof(InverseTAMP)), "..", "examples", "blocksworld")
+    scene_graph_before = pddl_to_scene_graph(state)
+    visualize(scene_graph_before, save_path, "before")
+
     state = execute(@julog(pickup(table, block1)), state, domain)
     @test satisfy(@julog(holding(block1)), state, domain)[1] == true
+    @test satisfy(problem.goal, state, domain)[1] == true
+
+    scene_graph_after = pddl_to_scene_graph(state)
+    visualize(scene_graph_after, save_path, "after")
+end
+
+function test2()
+    path = joinpath(dirname(pathof(InverseTAMP)), "..", "domains", "tabletop")
+    @info "Path is $path"
+
+    domain = load_domain(joinpath(path, "domain.pddl"))
+    @test domain.name == Symbol("blocks")
+    @test domain.predicates[:holding] == @julog(holding(X))
+    @test @julog(handempty <<= forall(and(block(X)), not(holding(X)))) in domain.axioms
+
+    problem = load_problem(joinpath(path, "problem-4.pddl"))
+    @test problem.name == Symbol("blocks-4")
+    @test problem.objects == @julog [table, block1, block2, block3]
+
+    state = initialize(problem)
+    @test satisfy(@julog(on(table, block1)), state, domain)[1] == true
+    @test satisfy(@julog(handempty), state, domain)[1] == true
+    @test satisfy(@julog(amounton(block1) == 0), state, domain)[1] == true
+    @test available(@julog(pickup(table, block1)), state, domain)[1] == true
+
+    save_path = joinpath(dirname(pathof(InverseTAMP)), "..", "examples", "blocksworld", "block_tower")
+    scene_graph_1 = pddl_to_scene_graph(state)
+    visualize(scene_graph_1, save_path, "001")
+
+    state = execute(@julog(pickup(table, block2)), state, domain)
+    @test satisfy(@julog(holding(block2)), state, domain)[1] == true
+    @test satisfy(@julog(fits(block1, block2)), state, domain)[1] == true
+    @test available(@julog(putdown(block1, block2)), state, domain)[1] == true
+
+    scene_graph_2 = pddl_to_scene_graph(state)
+    visualize(scene_graph_2, save_path, "002")
+
+    state = execute(@julog(putdown(block1, block2)), state, domain)
+    @test satisfy(@julog(holding(block2)), state, domain)[1] == false
+
+    scene_graph_3 = pddl_to_scene_graph(state)
+    visualize(scene_graph_3, save_path, "003")
+
+    state = execute(@julog(pickup(table, block3)), state, domain)
+    @test satisfy(@julog(holding(block3)), state, domain)[1] == true
+
+    scene_graph_4 = pddl_to_scene_graph(state)
+    visualize(scene_graph_4, save_path, "004")
+
+    state = execute(@julog(putdown(block2, block3)), state, domain)
+    @test satisfy(@julog(holding(block3)), state, domain)[1] == false
+
+    scene_graph_5 = pddl_to_scene_graph(state)
+    visualize(scene_graph_5, save_path, "005")
+
     @test satisfy(problem.goal, state, domain)[1] == true
 end
--- src/scene-graph-utils.jl
+++ src/scene-graph-utils.jl
@@ -1,53 +1,58 @@
 using InverseTAMP
 using ShapesWorld
+using PyPlot
 
 function pddl_to_scene_graph(state::State)
-    g = SceneGraph()
+    g = ShapesWorld.SceneGraph()
     facts = state.facts
     fluents = state.fluents
     objects = []
     # Get the names of all the block objects
     for term in facts
         if term.name == :block
-            push!(objects, term.args[1])
+            push!(objects, (term.args[1].name,))
         end
     end
 
     sizes = fluents[:size]
-
+    xs, ys, zs = fluents[:posx], fluents[:posy], fluents[:posz]
+    yaws, pitches, rolls = fluents[:yaw], fluents[:pitch], fluents[:roll]
     for (i, object) in enumerate(objects)
         size = sizes[object]
-        addObject!(g, Symbol("block", i), Box(size, size, size))
+        ShapesWorld.addObject!(g, Symbol("block", i), ShapesWorld.Box(size, size, size))
+        if fluents[:roll][object] != -1
+            ShapesWorld.setPose!(g, Symbol("block", i), [xs[object], ys[object], zs[object]],
+                        (yaw=yaws[object], pitch=pitches[object], roll=rolls[object]))
+        end
     end
 
     for fact in facts
         if fact.name == :on
             base, top = fact.args
-            x = fluents[:x][top]
-            y = fluents[:y][top]
-            theta = fluents[:theta][top]
-            setContact!(g, base, :top, (),
-                        top, :bottom, (),
+            base, top = (base.name,), (top.name,)
+            # Works since each object in objects is unique
+            base_idx = findfirst(x -> x == base, objects)
+            top_idx = findfirst(x -> x == top, objects)
+            x = xs[top]
+            y = ys[top]
+            theta = yaws[top]
+            ShapesWorld.setContact!(g, Symbol("block", base_idx), :top, (),
+                        Symbol("block", top_idx), :bottom, (),
                         x, y, theta)
-        elseif fact.name == :holding
-            block = fact.args[1]
-
-            setPose!()
         end
     end
 
-        if base == nothing
-            position =
-            orientation =
-            setPose!()
-        else
-
-        end
-    end
-
+    ShapesWorld.set_prop!(g, :blurWidth, 5)
+    ShapesWorld.set_prop!(g, :decayFactor, 0.1)
     return g
 end
 
 function scene_graph_to_pddl(scene_graph)
 
+end
+
+function visualize(scene_graph, outdir, frame_name_prefix)
+    (rgba, depth, seg, _, _) = ShapesWorld.renderScene(scene_graph)
+    png_file_path = joinpath(outdir, frame_name_prefix * ".png")
+    PyPlot.imsave(png_file_path, rgba)
 end
