f9576630a4947796fbdf0750b755a5f90027b3eb
Xuan <tanqazx@gmail.com>
2020-05-28 02:41:42 -0400

    Update FastDownward integration so that PyCall is not required.

--- src/planners/external.jl
+++ src/planners/external.jl
@@ -1,57 +1,50 @@
 export FastDownwardPlanner
 
-"FastDownward planner."
+"Wrapper to the FastDownward planning system."
 @kwdef struct FastDownwardPlanner <: Planner
-    timeout::Int = 10
-    max_nodes::Real = Inf
     domain_path::String
     problem_path::String
     heuristic::String = "add"
-    heuristic_params::Dict{String, String} = Dict()
+    h_params::Dict{String, String} = Dict()
+    timeout::Float64 = 10
+    verbose::Bool = false
 end
 
 set_max_resource(planner::FastDownwardPlanner, val) =
-    @set planner.max_nodes = val
+    @set planner.timeout = val
 
 get_call(::FastDownwardPlanner)::GenerativeFunction = fast_downward_call
 
 "Calls the FastDownward planning system to produce a plan."
 @gen function fast_downward_call(planner::FastDownwardPlanner, domain::Domain,
                                  state::State, goal_spec::GoalSpec)
-    @unpack timeout, max_nodes, heuristic, heuristic_params = planner
+    if !haskey(ENV, "FD_PATH")
+        error("FD_PATH not set to location of fast_downward.py") end
     @unpack domain_path, problem_path = planner
-
-    py"""
-    import sys, os, subprocess
-    def fd_wrapper(heuristic_with_params, timeout, domain_path, problem_path):
-        if 'FD_PATH' not in os.environ:
-            raise Exception((
-                "Environment variable `FD_PATH` not found. Make sure "
-                "FastDownward is installed and FD_PATH is set to the "
-                "path of fast-downward.py."
-            ))
-        FD_PATH = os.environ['FD_PATH']
-        timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} {} --search 'astar({})'".\
-            format(timeout_cmd, timeout, FD_PATH, domain_path,
-                   problem_path, heuristic_with_params)
-        output = subprocess.getoutput(cmd_str)
-        if "Solution found" not in output:
-            return None
-        plan = []
-        with open("sas_plan") as f:
-            plan = f.readlines()[:-1]
-        os.remove("sas_plan")
-        return plan
-    """
-
-    params = join(["$key=$val" for (key, val) in heuristic_params], ", ")
-    heuristic_with_params = "$heuristic($params)"
-    plan = py"fd_wrapper"(heuristic_with_params, timeout,
-                          domain_path, problem_path)
-
-    if (plan == nothing) return nothing, nothing end
-    plan = [(parse_pddl(act[1:end-1])) for act in plan]
+    @unpack heuristic, h_params, timeout, verbose = planner
+    # Set up shell command to fast_downward.py
+    fd_path = ENV["FD_PATH"]
+    py_cmd = get(ENV, "PYTHON", "python")
+    h_params = join(["$key=$val" for (key, val) in h_params], ", ")
+    search_params = "astar($heuristic($h_params))"
+    cmd = `$py_cmd $fd_path $domain_path $problem_path --search $search_params`
+    # Run command up to timeout
+    out = Pipe()
+    proc = run(pipeline(cmd, stdout=out); wait=false)
+    cb() = process_exited(proc)
+    timedwait(cb, float(timeout))
+    if process_running(proc)
+        kill(proc); close(out.in); return nothing, nothing end
+    # Read output and check if solution was found
+    close(out.in)
+    output = read(out, String)
+    if verbose println(output) end
+    if !occursin("Solution found", output)
+        return nothing, nothing end
+    # Read plan from file
+    plan = readlines("./sas_plan")[1:end-1]
+    Base.Filesystem.rm("./sas_plan")
+    plan = parse_pddl.(plan)
     traj = PDDL.simulate(domain, state, plan)
     return plan, traj
 end
