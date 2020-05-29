fe3a56bc2c926e8eb35115393fc419fb3b4b6d7f
Jordyn Mann <jordynm@mit.edu>
2020-05-28 23:10:33 -0400

    Implement block-words RNN input conversion

--- src/utils.jl
+++ src/utils.jl
@@ -39,3 +39,43 @@ dist_type(d::Distribution{T}) where {T} = T
         return fn
     end
 end
+
+"Convert from block-words PDDL state representation to RNN input representation"
+function block_words_RNN_conversion(state::State)
+    blocks = [term.args[1] for term in types]
+    n = length(blocks)
+    # on requires size porportional to square of the number of blocks,
+    # ontable, clear, and holding require size proportional to number of blocks,
+    # and handempty requires constant size
+    dimension = n ^ 2 + 3 * n + 1
+    encoding = zeros(dimension)
+    facts = state.facts
+    for fact in facts
+        if fact.name == :on
+            top, base = fact.args
+            idx = (n - 1) * findfirst(isequal(top), blocks) + findfirst(isequal(base), blocks)
+        elseif fact.name == :ontable
+            block = fact.args[1]
+            idx = n ^ 2 + findfirst(isequal(block), blocks)
+        elseif fact.name == :clear
+            block = fact.args[1]
+            idx = n ^ 2 + n + findfirst(isequal(block), blocks)
+        elseif fact.name == :holding
+            block = fact.args[1]
+            idx = n ^ 2 + 2 * n + findfirst(isequal(block), blocks)
+        elseif fact.name == :handempty
+            idx = n ^ 2 + 3 * n + 1
+        end
+        encoding[idx] = 1
+    end
+    return encoding
+end
+
+"Convert from gems, keys, doors PDDL state representation to RNN input representation"
+function gems_keys_doors_RNN_conversion(state::State)
+    encoding = []
+    types = state.types
+    facts = state.facts
+    fluents = state.fluents
+    return encoding
+end
