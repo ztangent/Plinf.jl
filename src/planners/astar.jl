3eb7007e0555d9a27d155682ccc2cf9ed2f007bc
Xuan <tanqazx@gmail.com>
2020-06-16 00:11:35 -0400

    Add @info msgs so node counts can be measured by experiment code.

--- src/planners/astar.jl
+++ src/planners/astar.jl
@@ -46,6 +46,7 @@ get_call(::AStarPlanner)::GenerativeFunction = astar_call
         if trace_states @trace(labeled_unif([state]), (:state, count)) end
         # Return plan if search budget is reached or goals are satisfied
         if count >= max_nodes || satisfy(goals, state, domain)[1]
+            @info "Node Count: $count"
             return reconstruct_plan(state_hash, state_dict, parents) end
         count += 1
         # Get list of available actions
@@ -121,6 +122,7 @@ get_call(::ProbAStarPlanner)::GenerativeFunction = aprob_call
         delete!(queue, state_hash)
         # Return plan if search budget is reached or goals are satisfied
         if count >= max_nodes || satisfy(goals, state, domain)[1]
+            @info "Node Count: $count"
             return reconstruct_plan(state_hash, state_dict, parents)
         end
         count += 1
