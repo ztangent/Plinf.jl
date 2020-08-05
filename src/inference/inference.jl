7d0116a24488861966f093d3dce43f18528fa7d4
Xuan <tanqazx@gmail.com>
2020-08-04 22:11:06 -0400

    Rename lookahead to delay.

--- src/inference/inference.jl
+++ src/inference/inference.jl
@@ -55,21 +55,26 @@ end
 function world_particle_filter(
         world_init::WorldInit, world_config::WorldConfig,
         obs_traj::Vector{State}, obs_terms::Vector{<:Term}, n_particles::Int;
-        batch_size::Int=1, lookahead::Int=0, strata=nothing, callback=nothing,
+        batch_size::Int=1, delay::Int=0, strata=nothing, callback=nothing,
         ess_threshold::Float64=1/4, update_proposal=nothing,
         resample=true, rejuvenate=nothing)
     # Construct choicemaps from observed trajectory
     @unpack domain = world_config
     n_obs = length(obs_traj)
     obs_choices = traj_choicemaps(obs_traj, domain, obs_terms;
-                                  batch_size=batch_size, offset=lookahead)
+                                  batch_size=batch_size, offset=delay)
     # Initialize particle filter
     world_args = (world_init, world_config)
     argdiffs = (UnknownChange(), NoChange(), NoChange())
     pf_state =  initialize_pf_stratified(world_model, (0, world_args...),
                                          choicemap(), strata, n_particles)
+    # Run callback for initial states if delay is used
+    if delay > 0 && callback != nothing
+        traces, weights = get_traces(pf_state), get_log_norm_weights(pf_state)
+        for t=1:delay callback(t, obs_traj[t], traces, weights) end
+    end
     # Compute times for each batch
-    timesteps = collect((batch_size+lookahead):batch_size:n_obs)
+    timesteps = collect(batch_size+delay:batch_size:n_obs)
     if timesteps[end] < n_obs push!(timesteps, n_obs) end
     # Feed new observations batch-wise
     for (batch_i, t) in enumerate(timesteps)
@@ -91,17 +96,11 @@ function world_particle_filter(
         end
         if callback != nothing # Run callback on current traces
             trs, ws = get_traces(pf_state), get_log_norm_weights(pf_state)
-            callback(t-lookahead, obs_traj[t-lookahead], trs, ws)
+            callback(t, obs_traj[t], trs, ws)
         end
     end
     # Return particles and their weights
     traces, weights = get_traces(pf_state), get_log_norm_weights(pf_state)
     lml_est = logsumexp(get_log_weights(pf_state)) - log(n_particles)
-    # Run callback for remaining states if lookahead is used
-    if callback != nothing
-        for t in (n_obs-lookahead+1):n_obs
-            callback(t, obs_traj[t], traces, weights)
-        end
-    end
     return traces, weights, lml_est
 end
