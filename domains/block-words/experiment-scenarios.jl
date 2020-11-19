4dbf0d6656c57ac51cfc07b297150aa2ed3d702c
Joie Le <joiele@mit.edu>
2020-11-18 22:33:45 -0800

    Generate json data for experiment 4

--- domains/block-words/experiment-scenarios.jl
+++ domains/block-words/experiment-scenarios.jl
@@ -1,40 +1,47 @@
 # hardcoded experiment sequences
 
+# Dictionary of Actions
+action_dict = Dict()
+
 #experiment 1a - backtracking
-actions = ["(unstack o e)", "(stack o w)", "(unstack e c)",
-        "(stack e r)", "(unstack o w)", "(stack o c)",
-        "(unstack w p)", "(stack w e)", "(unstack o c)",
-        "(stack o w)", "(pick-up p)", "(stack p o)"]
+action_dict["1a"] = ["(unstack o e)", "(stack o w)", "(unstack e c)",
+                "(stack e r)", "(unstack o w)", "(stack o c)",
+                "(unstack w p)", "(stack w e)", "(unstack o c)",
+                "(stack o w)", "(pick-up p)", "(stack p o)"]
 
 #experiment 2- misspelled word
-actions = ["(unstack p r)", "(put-down p)", "(unstack c e)",
-        "(stack c p)", "(pick-up e)", "(stack e r)",
-        "(pick-up o)", "(stack o e)", "(pick-up w)",
-        "(stack w o)", "(unstack c p)", "(stack c w)",
-        "(unstack c w)", "(put-down c)", "(unstack w o)",
-        "(put-down w)", "(unstack o e)", "(put-down o)",
-        "(pick-up w)", "(stack w e)", "(pick-up o)",
-        "(stack o w)", "(pick-up c)",
-        "(stack c o)"]
+action_dict["2"] = ["(unstack p r)", "(put-down p)", "(unstack c e)",
+                "(stack c p)", "(pick-up e)", "(stack e r)",
+                "(pick-up o)", "(stack o e)", "(pick-up w)",
+                "(stack w o)", "(unstack c p)", "(stack c w)",
+                "(unstack c w)", "(put-down c)", "(unstack w o)",
+                "(put-down w)", "(unstack o e)", "(put-down o)",
+                "(pick-up w)", "(stack w e)", "(pick-up o)",
+                "(stack o w)", "(pick-up c)", "(stack c o)"]
 
 #experiment 3- moving irrelevant blocks
-actions = ["(pick-up a)", "(stack a b)", "(unstack a b)",
-        "(put-down a)", "(pick-up b)", "(stack b a)",
-        "(unstack b a)", "(put-down b)"]
+action_dict["3a"] = ["(pick-up a)", "(stack a b)", "(unstack a b)",
+                "(put-down a)", "(pick-up b)", "(stack b a)",
+                "(unstack b a)", "(put-down b)"]
 
 #experiment 4a - initial configuration matches goal state
-actions = ["(unstack c r)", "(stack c p)", "(unstack r o)",
-        "(stack r e)", "(unstack o w)", "(stack o r)",
-        "(unstack c p)", "(stack c o)"]
+action_dict["4a"]= ["(unstack c r)", "(stack c p)", "(unstack r o)",
+                "(stack r e)", "(unstack o w)", "(stack o r)",
+                "(unstack c p)", "(stack c o)"]
 
 #experiment 4b1 - intermediate matching goal
-actions = ["(unstack a r)", "(stack a w)", "(pick-up r)",
-        "(stack r a)", "(unstack d e)", "(stack d r)"]
+action_dict["4b1"] = ["(unstack a r)", "(stack a w)", "(pick-up r)",
+                "(stack r a)", "(unstack d e)", "(stack d r)"]
 
 #experiment 4b2 - intermediate matching goal
-actions = ["(unstack a w)", "(stack a r)", "(unstack e p)",
-        "(stack e a)", "(pick-up w)", "(stack w e)"]
+action_dict["4b2"] = ["(unstack a w)", "(stack a r)", "(unstack e p)",
+                "(stack e a)", "(pick-up w)", "(stack w e)"]
 
 #experiment 4c - rhyming suffix (not a goal state)
-actions = ["(unstack a e)", "(stack a r)", "(unstack e p)",
-        "(stack e a)", "(pick-up w)", "(stack w e)"]
+action_dict["4c"]= ["(unstack a e)", "(stack a r)", "(unstack e p)",
+                "(stack e a)", "(pick-up w)", "(stack w e)"]
+
+# Get action list for respective action
+function get_action(action)
+        return action_dict[action]
+end
--- domains/block-words/render-scenarios.jl
+++ domains/block-words/render-scenarios.jl
@@ -11,46 +11,54 @@
     # (stack a b) put block a on top of block b
     # Preconditions: block a must be already held; block b must be clear;
 
-using Julog, PDDL, Gen, Printf
+using Julog, PDDL, Gen, Printf, JSON
 using Plinf
 
 include("render.jl")
 include("utils.jl")
-
+include("experiment-scenarios.jl")
 
 # Specify problem name and your list of actions
-experiment_number = "4"
-experiment_problem = "b2"
-problem_name = "experiment-" * experiment_number * experiment_problem * ".pddl"
+category = "4"
+subcategory = "c"
+experiment = "experiment-" * category * subcategory
+problem_name =  experiment * ".pddl"
 
-actions = ["(unstack a w)", "(stack a r)", "(unstack e p)",
-        "(stack e a)", "(pick-up w)", "(stack w e)"]
+actions = get_action(category * subcategory)
 
 # Load domain and problem
 path = joinpath(dirname(pathof(Plinf)), "..", "domains", "block-words")
-save_image_path = joinpath(path, "experiments-frames")
 domain = load_domain(joinpath(path, "domain.pddl"))
 problem = load_problem(joinpath(path, problem_name))
+
+save_image_path = joinpath(path, "experiments-frames", category, subcategory)
 
 # Initialize problem and visualize initial state
 state = initialize(problem)
 goal = problem.goal
 plt = render(state)
 
+# Initialize items for json file
+json_dict =  Dict()
+json_dict["goal"] = goal
+json_dict["actions"] = actions
+
 # Execute list of actions and generate intermediate states
 function execute_plan(state, domain, actions)
     states = State[]
     timestep = 0
     push!(states, state)
-    png(render(state), joinpath(save_image_path, experiment_number,
-                                experiment_problem, string(timestep)))
+
+    json_dict[string(timestep)] = state
+    png(render(state), joinpath(save_image_path, string(timestep)))
+
     for action in actions
         print(action)
         action = parse_pddl(action)
         state = execute(action, state, domain)
         timestep += 1
-        png(render(state), joinpath(save_image_path, experiment_number,
-                                    experiment_problem, string(timestep)))
+        json_dict[string(timestep)] = state
+        png(render(state), joinpath(save_image_path, string(timestep)))
         push!(states, state)
     end
     return states
@@ -59,6 +67,12 @@ end
 
 traj_s = execute_plan(state, domain, actions)
 anim = anim_traj(traj_s)
+
+json_data = JSON.json(json_dict)
+json_file = joinpath(save_image_path, experiment * ".json")
+open(json_file, "w") do f
+    JSON.print(f, json_data)
+end
 
 gif(anim, "domains/block-words/experiments-gifs/experiment-" *
             experiment_number * experiment_problem * ".gif", fps=30)
