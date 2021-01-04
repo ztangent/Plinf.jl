393f55047d6e4909f3a6f5bc1d0ceb7bf9f5830b
Xuan <tanqazx@gmail.com>
2021-01-04 16:22:40 -0500

    Bugfix to replan_move_proposal (caused by error with get_planning_steps).

--- src/planners/replanners.jl
+++ src/planners/replanners.jl
@@ -34,7 +34,8 @@ end
 
 "Find all timesteps where replanning occurred."
 function get_planning_steps(rp_states::AbstractArray{ReplanState})
-    unzip([(t, rp) for (t, rp) in enumerate(rp_states) if rp.rel_step == 1])
+    unzip([(t, rp) for (t, rp) in enumerate(rp_states)
+           if rp.rel_step == 1 && !rp.plan_done])
 end
 
 "Find the most recent timestep at which replanning occured."
