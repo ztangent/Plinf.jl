2926c8a11c5e6372afc57162423e73c38ca4ef08
Joie Le <joiele@mit.edu>
2021-01-30 10:44:29 -0800

    Add new plans

--- domains/doors-keys-gems/stimuli.jl
+++ domains/doors-keys-gems/stimuli.jl
@@ -12,14 +12,16 @@ GEM_COLORS = Dict(zip(
     [colorant"#D41159", colorant"#FFC20A", colorant"#1A85FF"]
 ))
 
-prob_goal_trial= Dict("6_1_0" => ("1","1"), "7_0_0" => ("1","2"), "4_0_0" => ("1","3"), "12_2_0" => ("1","4"),
-                    "9_2_0" => ("2","1"), "5_0_0" => ("2","2"), "10_2_0" => ("2","3"), "11_1_1" => ("2","4"),
-                    "6_0_0" => ("3","1"), "4_1_0" => ("3","2"), "11_1_0" => ("3","3"), "5_2_1" => ("3","4"),
-                    "7_1_0" => ("4","1"), "5_2_0" => ("4","2"), "10_0_0" => ("4","3"), "12_2_1" => ("4","4"),
-                    "8_1_0" => ("2", "4"), "8_2_0" => ("3", "4"))
+# prob_goal_trial= Dict("6_1_0" => ("1","1"), "7_0_0" => ("1","2"), "4_0_0" => ("1","3"), "12_2_0" => ("1","4"),
+#                     "9_2_0" => ("2","1"), "5_0_0" => ("2","2"), "10_2_0" => ("2","3"), "11_1_1" => ("2","4"),
+#                     "6_0_0" => ("3","1"), "4_1_0" => ("3","2"), "11_1_0" => ("3","3"), "5_2_1" => ("3","4"),
+#                     "7_1_0" => ("4","1"), "5_2_0" => ("4","2"), "10_0_0" => ("4","3"), "12_2_1" => ("4","4"),
+#                     "8_1_0" => ("2", "4"), "8_2_0" => ("3", "4"))
 
-splitpoints_dict = Dict("1_1" => [7,14,20], "1_2" => [9,14,17], "1_3" => [11,17], "1_4" => [7,15,23,32],
-                        "2_1" => [6,11,18], "2_2" => [6,11], "2_3" => [5,8,13], "2_4" => [9,12,18,26,31,41])
+splitpoints_dict = Dict("1_1" => [7,14,20], "1_2" => [9,14,17], "1_3" => [9,17,24], "1_4" => [7,14,23,32],
+                        "2_1" => [6,11,21], "2_2" => [6,11], "2_3" => [5,8,13], "2_4" => [9,12,31,44],
+                        "3_1" => [7,22,37], "3_2" => [14,24,29], "3_3" => [7,13,20], "3_4" => [6,11,47],
+                        "4_1" => [8,14,18], "4_2" => [4,8], "4_3" => [5,8], "4_4" => [7,12,18])
 
 function get_ids_from_fn(fn)
     m = match(r".*(\d+)_(\d+)_problem_(\d+)_goal(\d+)_(\d+).*", fn)
@@ -67,17 +69,20 @@ function generate_stimuli(plan_dir; seg_len=3)
         #     end
         # end
         # splitpoints = collect(1:seg_len:length(traj))
-        if splitpoints[end] != length(traj) push!(splitpoints, length(traj)) end
-        splitpoints = splitpoints_dict[scenario * "_" * number]
-        anims = anim_traj(traj; gem_colors=GEM_COLORS, splitpoints=splitpoints,
-                                  start_pos=start_pos, plan=plan, show=false)
-        for (j, a) in enumerate(anims)
-            gif(a, stim_path * "/$(stim_name)_$(j-1).gif"; fps=3, loop=-1)
-        end
+        splitpoints = splitpoints_dict[string(scenario) * "_" * string(number)]
+        pushfirst!(splitpoints, 1)
+        print(splitpoints)
+        # if splitpoints[end] != length(traj) push!(splitpoints, length(traj)) end
 
-        anim = anim_traj(traj; gem_colors=GEM_COLORS, start_pos=start_pos,
-                            plan=plan, show=false)
-        gif(anim, stim_path * "/$(stim_name).gif"; fps=3, loop=-1)
+        # anims = anim_traj(traj; gem_colors=GEM_COLORS, splitpoints=splitpoints,
+        #                           start_pos=start_pos, plan=plan, show=false)
+        # for (j, a) in enumerate(anims)
+        #     gif(a, stim_path * "/$(stim_name)_$(j-1).gif"; fps=3, loop=-1)
+        # end
+        #
+        # anim = anim_traj(traj; gem_colors=GEM_COLORS, start_pos=start_pos,
+        #                     plan=plan, show=false)
+        # gif(anim, stim_path * "/$(stim_name).gif"; fps=3, loop=-1)
 
         # traj_split = [traj[i:min(i + seg_len - 1, end)] for i in 1:seg_len:length(traj)]
         # plan_split = [plan[i:min(i + seg_len - 1, end)] for i in 1:seg_len:length(plan)]
@@ -95,8 +100,13 @@ function generate_stimuli(plan_dir; seg_len=3)
 
         m[:name] = stim_name
         m[:length] = length(splitpoints)
+        # if scenario == 4
+        #     m[:length] = length(splitpoints)
+        # else
+        #     m[:length] = length(splitpoints)-1
+        # end
         m[:times] = splitpoints
-        m[:images] = ["stimuli/" * stim_name * "_$(j-1).gif" for j=1:m[:length]]
+        m[:images] = ["stimuli/" * stim_name * "_$(j-1).gif" for j=1:m[:length]+1]
     end
     open(stim_path * "/stimuli.json","w") do f
         JSON.print(f, metadata, 2)
