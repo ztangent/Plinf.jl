6fa80664df81a82f476f19c3a4d599abe027301f
gzlin7 <gloriazlin@yahoo.com>
2021-02-01 22:19:48 -0500

    add grid search for DKG

--- domains/doors-keys-gems/grid_search.jl
+++ domains/doors-keys-gems/grid_search.jl
@@ -6,7 +6,6 @@ using JSON
 
 include("render.jl")
 include("utils.jl")
-# include("./new-scenarios/experiment-scenarios.jl")
 
 #--- Generate Search Grid ---#
 model_name = "ap"
@@ -25,13 +24,34 @@ for item in grid_list
     push!(grid_dict, current_dict)
 end
 
+# omit 1st judgement point
+judgement_points =
+[[7,14,20],
+[9,14,17],
+[9,17,24],
+[7,14,23,32],
+[6,11,21],
+[6,11],
+[5,8,13],
+[9,12,31,44],
+[7,22,37],
+[14,24,29],
+[7,13,20],
+[6,11,47],
+[8,14,18],
+[4,8],
+[5,8],
+[7,12,18]
+]
+
 
 #--- Initial Setup ---#
 
 # Specify problem
-category = "1"
+category = "2"
 subcategory = "1"
-corrolation = []
+stimulus_idx = ((parse(Int64,category)-1) * 4) +  parse(Int64,subcategory)
+correlation = []
 experiment = "scenario-" * category * "-" * subcategory
 problem_name = experiment * ".pddl"
 path = joinpath(dirname(pathof(Plinf)), "..", "domains", "doors-keys-gems")
@@ -48,6 +68,9 @@ file_name = category * "_" * subcategory * ".dat"
 actions = readlines(joinpath(path, "new-scenarios", "actions" ,file_name))
 goals = @julog [gem1, gem2, gem3]
 
+goal_colors = [colorant"#D41159", colorant"#FFC20A", colorant"#1A85FF"]
+gem_terms = @julog [gem1, gem2, gem3]
+gem_colors = Dict(zip(gem_terms, goal_colors))
 
 # Initialize state
 state = initialize(problem)
@@ -67,84 +90,100 @@ function execute_plan(state, domain, actions)
 end
 
 traj = execute_plan(state, domain, actions)
+print(size(traj))
 
 
 
 #--- Goal Inference ---#
 
 function goal_inference(params, domain, problem, goal_words, goals, state, traj)
-    #### Goal Inference Setup ####
-
-    # Goal noise implementation
+    #--- Goal Inference Setup ---#
+    # Specify possible goals
+    goals = [@julog([has(gem1)]), @julog([has(gem2)]), @julog([has(gem3)])]
+    goal_idxs = collect(1:length(goals))
+    goal_names = [repr(t[1]) for t in goals]
 
     # Define uniform prior over possible goals
     @gen function goal_prior()
-        GoalSpec(word_to_terms(@trace(labeled_unif(goal_words), :goal)))
+        GoalSpec(goals[@trace(uniform_discrete(1, length(goals)), :goal)])
     end
-    goal_strata = Dict((:goal_init => :goal) => goal_words)
+    goal_strata = Dict((:init => :agent => :goal => :goal) => goal_idxs)
 
     # Assume either a planning agent or replanning agent as a model
-    heuristic = precompute(FFHeuristic(), domain)
-    planner = ProbAStarPlanner(heuristic=heuristic, search_noise=params["search_noise"])
+    planner = ProbAStarPlanner(heuristic=GemManhattan(), search_noise=0.1)
+    # planner = ProbAStarPlanner(heuristic=GemMazeDist(), search_noise=0.1)
+    # TODO: change to maze dist heuristic!!
     replanner = Replanner(planner=planner, persistence=(2, 0.95))
     agent_planner = replanner # planner
 
     # Configure agent model with goal prior and planner
+    act_noise = 0.05 # Assume a small amount of action noise
     agent_init = AgentInit(agent_planner, goal_prior)
-    agent_config = AgentConfig(domain, agent_planner, act_noise=params["action_noise"])
+    agent_config = AgentConfig(domain, agent_planner, act_noise=0.05)
 
     # Define observation noise model
-    obs_params = observe_params(domain, pred_noise=params["pred_noise"]; state=state)
+    obs_params = observe_params(
+        (@julog(xpos), normal, 0.25), (@julog(ypos), normal, 0.25),
+        (@julog(forall(doorloc(X, Y), door(X, Y))), 0.05),
+        (@julog(forall(item(Obj),has(Obj))), 0.05),
+        (@julog(forall(and(item(Obj), itemloc(X, Y)), at(Obj, X, Y))), 0.05)
+    )
     obs_terms = collect(keys(obs_params))
 
     # Configure world model with planner, goal prior, initial state, and obs params
     world_init = WorldInit(agent_init, state, state)
     world_config = WorldConfig(domain, agent_config, obs_params)
 
+    #--- Online Goal Inference ---#
 
-    #### Online Goal Inference ####
-
-    # Set up visualization and logging callbacks for online goal inference
     goal_probs = [] # Buffer of goal probabilities over time
     callback = (t, s, trs, ws) -> begin
-        # print("t=$t\t");
-        push!(goal_probs, return_goal_probs(get_goal_probs(trs, ws, goal_words)));
-        # print_goal_probs(get_goal_probs(trs, ws, goal_words))
+        goal_probs_t = collect(values(sort!(get_goal_probs(trs, ws, goal_idxs))))
+        push!(goal_probs, goal_probs_t)
+        # print("t=$t\t")
+        # print_goal_probs(get_goal_probs(trs, ws, goal_idxs))
     end
 
-    act_proposal = params["action_noise"] > 0 ? forward_act_proposal : nothing
-    act_proposal_args = (params["action_noise"],)
-
     # Set up rejuvenation moves
-    goal_rejuv! = pf -> pf_goal_move_accept!(pf, goal_words)
+    goal_rejuv! = pf -> pf_goal_move_accept!(pf, goals)
     plan_rejuv! = pf -> pf_replan_move_accept!(pf)
-    mixed_rejuv! = pf -> pf_mixed_move_accept!(pf, goal_words; mix_prob=0.25)
+    mixed_rejuv! = pf -> pf_mixed_move_accept!(pf, goals; mix_prob=0.25)
 
+    # Set up action proposal to handle potential action noise
+    act_proposal = act_noise > 0 ? forward_act_proposal : nothing
+    act_proposal_args = (act_noise,)
+
+    # Run a particle filter to perform online goal inference
+    n_samples = 30
     traces, weights =
-        world_particle_filter(world_init, world_config, traj, obs_terms, params["n_samples"];
-                              resample=true, rejuvenate=nothing,
-                              strata=goal_strata, callback=callback,
+        world_particle_filter(world_init, world_config, traj, obs_terms, n_samples;
+                              resample=true, rejuvenate=mixed_rejuv!, strata=goal_strata,
+                              callback=callback,
                               act_proposal=act_proposal,
                               act_proposal_args=act_proposal_args)
-
-    # Show animation of goal inference
-    #gif(anim, joinpath(path, "sips-results", experiment*".gif"), fps=1)
-
-    df = DataFrame(Timestep=collect(1:length(traj)), Probs=goal_probs)
-    # CSV.write(joinpath(path, "sips-results", experiment*".csv"), df)
     return goal_probs
 end
 
 for (i, params) in enumerate(grid_dict)
     total = size(grid_dict)[1]
     goal_probs = goal_inference(params, domain, problem, goal_words, goals, state, traj)
-    flattened_array = collect(Iterators.flatten(goal_probs[1:2:end]))
-    push!(corrolation, cor(flattened_array, human_data))
+    flattened_array = collect(Iterators.flatten(goal_probs[1:end]))
+    println(flattened_array)
+    # only correlate model predictions at judgement points
+    only_judgement_model = []
+    for i in judgement_points[stimulus_idx]
+        idx = (i) * 3
+        for j in flattened_array[idx-2:idx]
+            push!(only_judgement_model, j)
+        end
+    end
+    # skip first human datapoint
+    push!(correlation, cor(only_judgement_model, human_data[4:end]))
     print("Parameters Set " * string(i) * " / " * string(total) * " done \n")
 end
 
 #--- Save Best Parameters ---#
-mxval, mxindx = findmax(corrolation)
+mxval, mxindx = findmax(correlation)
 best_params = grid_dict[mxindx]
 best_params["corr"] = mxval
 json_data = JSON.json(best_params)
---
+++ domains/doors-keys-gems/grid_search_sph.jl
@@ -0,0 +1,131 @@
+using Julog, PDDL, Gen, Printf
+using Plinf, CSV
+using DataFrames
+using Statistics
+using JSON
+
+include("render.jl")
+include("utils.jl")
+
+
+#--- Generate Search Grid ---#
+model_name = "sph"
+beta_ = [0.1, 0.5, 0.9, 1.5, 3]
+grid_list = Iterators.product(beta_)
+grid_dict = []
+for item in grid_list
+    current_dict = Dict()
+    current_dict["beta"] = item[1]
+    push!(grid_dict, current_dict)
+end
+
+#--- Initial Setup ---#
+
+# Specify problem
+category = "4"
+subcategory = "4"
+corrolation = []
+experiment = "scenario-" * category * "-" * subcategory
+problem_name = experiment * ".pddl"
+path = joinpath(dirname(pathof(Plinf)), "..", "domains", "doors-keys-gems")
+
+goal_colors = [colorant"#D41159", colorant"#FFC20A", colorant"#1A85FF"]
+gem_terms = @julog [gem1, gem2, gem3]
+gem_colors = Dict(zip(gem_terms, goal_colors))
+
+# Load human data
+file_name = category * "_" * subcategory * ".csv"
+human_data = vec(CSV.read(joinpath(path, "human_results_arrays", file_name), datarow=1, Tables.matrix))
+
+# Load domain, problem, actions, and goal space
+domain = load_domain(joinpath(path, "domain.pddl"))
+problem = load_problem(joinpath(path, "new-scenarios", problem_name))
+file_name = category * "_" * subcategory * ".dat"
+actions = readlines(joinpath(path, "new-scenarios", "actions" ,file_name))
+goals = @julog [gem1, gem2, gem3]
+
+# Initialize state
+state = initialize(problem)
+start_pos = (state[:xpos], state[:ypos])
+goal = [problem.goal]
+
+
+# Execute list of actions and generate intermediate states
+function execute_plan(state, domain, actions)
+    states = State[]
+    push!(states, state)
+    for action in actions
+        action = parse_pddl(action)
+        state = execute(action, state, domain)
+        push!(states, state)
+    end
+    return states
+end
+traj = execute_plan(state, domain, actions)
+
+#-- Shortest path heuristic inference --#
+
+"Run goal inference via shortest-path heuristic."
+function run_sph_inference(traj::Vector{State}, goals, domain::Domain;
+                           beta::Real=1, verbose::Bool=true)
+    # Construct new dataframe for this trajectory
+    n_goals = length(goals)
+    goals = [GoalSpec(g) for g in goals]
+    all_goal_probs = [] # Buffer of all goal probabilities over time
+
+    # Compute costs of optimal plans from initial state to each goal
+    state = traj[1]
+    heuristic = precompute(FFHeuristic(), domain) # Change to GemMazeDist for DKG
+    planner = AStarPlanner(heuristic=heuristic, max_nodes=1000)
+
+    # Iterate over timesteps
+    if verbose println("Goal probs.:") end
+    for (t, state) in enumerate(traj)
+        weights = ones(n_goals)
+        costs = ones(n_goals)
+        for (i, g) in enumerate(goals)
+            if t == 1 break end
+            # Compute plan cost to each goal
+            part_plan, part_traj = planner(domain, state, g)
+            costs[i] = part_plan == nothing ? Inf : length(part_plan)
+            # Compute heuristic likelihood weight of the plan as cost ratio
+            weights[i] = exp(-beta * costs[i])
+        end
+        # Normalize weights to get posterior
+        if iszero(weights) weights = ones(n_goals) end # Handle failed plans
+        goal_probs = weights ./ sum(weights)
+        if verbose
+            for prob in goal_probs @printf("%.3f\t", prob) end
+            print("\n")
+        end
+        push!(all_goal_probs, goal_probs)
+    end
+    return all_goal_probs
+end
+
+for (i, params) in enumerate(grid_dict)
+    total = size(grid_dict)[1]
+    goal_probs = run_sph_inference(traj, goals, domain; beta = params["beta"])
+    flattened_array = collect(Iterators.flatten(goal_probs[1:2:end]))
+    push!(corrolation, cor(flattened_array, human_data))
+    print("Parameters Set " * string(i) * " / " * string(total) * " done \n")
+end
+
+#--- Save Best Parameters ---#
+mxval, mxindx = findmax(corrolation)
+best_params = grid_dict[mxindx]
+best_params["corr"] = mxval
+json_data = JSON.json(best_params)
+json_file = joinpath(path, model_name, "best_params", experiment*".json")
+open(json_file, "w") do f
+    JSON.print(f, json_data)
+end
+
+
+#--- Generate Results ---#
+number_of_trials = 1
+for i in 1:number_of_trials
+    goal_probs = run_sph_inference(traj, goals, domain; beta = best_params["beta"])
+    df = DataFrame(Timestep=collect(1:length(traj)), Probs=goal_probs)
+    CSV.write(joinpath(path, model_name, category * "_" * subcategory, string(i)*".csv"), df)
+end
