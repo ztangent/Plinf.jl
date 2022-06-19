7586a36112e1005b6fbffbf851fad2ef98a0d308
Xuan <tanqazx@gmail.com>
2022-06-18 22:40:33 -0400

    Port `find_matches` from PDDL.jl.

--- src/utils.jl
+++ src/utils.jl
@@ -48,6 +48,26 @@ dist_type(d::Distribution{T}) where {T} = T
 end
 
 """
+    find_matches(term, state, domain=nothing)
+
+Returns a list of all matching substitutions of `term` with respect to
+a given `state` and `domain`.
+"""
+function find_matches(domain::GenericDomain, state::GenericState, term::Term)
+    if term.name in keys(state.values)
+        clauses = Vector{Clause}(get_fluents(state))
+        _, subst = resolve(term, clauses; mode=:all)
+    else
+        clauses = isnothing(domain) ? Clause[] : get_clauses(domain)
+        clauses = Clause[clauses; collect(state.types); collect(state.facts)]
+        funcs = state.values
+        _, subst = resolve(term, clauses; funcs=funcs, mode=:all)
+    end
+    matches = Term[substitute(term, s) for s in subst]
+    return matches
+end
+
+"""
     simulate(domain, state, actions; kwargs...)
 Returns the state trajectory that results from applying a sequence of `actions`
 to an initial `state` in a given `domain`. Keyword arguments specify whether
