7f6837db7be3e92c880e930f37d4e17b9be035ac
Xuan <tanqazx@gmail.com>
2022-07-14 12:15:52 -0400

    Add GPT-3 completion script.

---
+++ domains/overcooked/gpt3_complete.jl
@@ -0,0 +1,28 @@
+using HTTP, JSON3
+
+function gpt3_complete(
+    prompt::String, n_completions::Int=1;
+    endpoint::String = "https://api.openai.com/v1/completions",
+    api_key::String = get(ENV, "OPENAI_API_KEY", ""),
+    model::String = "text-davinci-002",
+    temperature::Real = 0.7,
+    max_tokens::Int = 256,
+    logprobs::Union{Nothing,Int} = nothing,
+    echo::Bool = false,
+    stop::Union{String, Vector{<:String}} = "<|endoftext|>",
+    options... # Other options
+)
+    headers = ["Content-Type" => "application/json",
+               "Authorization" => "Bearer $api_key"]
+    data = Dict("prompt" => prompt,
+                "n" => n_completions,
+                "model" => model,
+                "temperature" => temperature,
+                "max_tokens" => max_tokens,
+                "logprobs" => logprobs,
+                "echo" => echo,
+                "stop" => stop, options...)
+    data = JSON3.write(data)
+    response = HTTP.post(endpoint, headers, data)
+    return JSON3.read(response.body)
+end
