e9190735828eb3fcf1c3ee2b8523b8d90bfda4ce
Xuan <tanqazx@gmail.com>
2022-10-24 19:30:00 -0400

    Update example plans for Overcooked problems.

--- domains/overcooked/example.jl
+++ domains/overcooked/example.jl
@@ -1,6 +1,6 @@
 using PDDL, SymbolicPlanners
 
-# Load domain and problem 1
+## Load domain and problem 1-1
 domain = load_domain(joinpath(@__DIR__, "domain.pddl"))
 problem = load_problem(joinpath(@__DIR__, "problem-1-1.pddl"))
 
@@ -16,21 +16,30 @@ state = execute(domain, state, pddl"(put-down knife1 chop-loc)")
 state = execute(domain, state, pddl"(take-out lettuce1 board1 chop-loc)")
 state = execute(domain, state, pddl"(move chop-loc plate-loc)")
 state = execute(domain, state, pddl"(place-in lettuce1 plate1 plate-loc)")
-satisfy(domain, state, problem.goal)
+@assert satisfy(domain, state, problem.goal)
 
-# Use planner to solve for goal
+# Use heuristic to check reachability
 state = initstate(domain, problem)
 heuristic = HMax()
 hval = heuristic(domain, state, problem.goal)
+
+# Use planner to solve for goal
 planner = AStarPlanner(HAdd(), save_search=true, max_time=300)
-@time sol = planner(domain, state, problem.goal);
+sol = planner(domain, state, problem.goal);
+
+# Check if solution was found
+@assert !(sol isa NullSolution)
 
 # Print solution
-write_pddl.(collect(sol)) |> display
+println("== Solution ==")
+for act in write_pddl.(collect(sol))
+    println(act)
+end
+println("Solution Length: $(length(sol))")
 
-# Load domain and problem 2
+##  Load domain and problem 2-1
 domain = load_domain(joinpath(@__DIR__, "domain.pddl"));
-problem = load_problem(joinpath(@__DIR__, "problem-3-1.pddl"));
+problem = load_problem(joinpath(@__DIR__, "problem-2-1.pddl"));
 
 # Manually execute plan and check if goal is satisfied
 state = initstate(domain, problem)
@@ -50,55 +59,94 @@ state = execute(domain, state, pddl"(move stove-loc plate-loc)")
 state = execute(domain, state, pddl"(transfer pot1 plate1)")
 state = execute(domain, state, pddl"(move plate-loc stove-loc)")
 state = execute(domain, state, pddl"(put-down pot1 stove-loc)")
-# Put sliced fish in the plate
+# Put sliced tuna in the plate
 state = execute(domain, state, pddl"(move stove-loc food-loc)")
-state = execute(domain, state, pddl"(pick-up fish1 food-loc)")
+state = execute(domain, state, pddl"(pick-up tuna1 food-loc)")
 state = execute(domain, state, pddl"(move food-loc chop-loc)")
-state = execute(domain, state, pddl"(place-in fish1 board1 chop-loc)")
-state = execute(domain, state, pddl"(pick-up knife1 chop-loc)")
-state = execute(domain, state, pddl"(prepare slice board1 knife1 fish1 chop-loc)")
-state = execute(domain, state, pddl"(put-down knife1 chop-loc)")
-state = execute(domain, state, pddl"(take-out fish1 board1 chop-loc)")
+state = execute(domain, state, pddl"(place-in tuna1 board1 chop-loc)")
+state = execute(domain, state, pddl"(pick-up s-knife1 chop-loc)")
+state = execute(domain, state, pddl"(prepare slice board1 s-knife1 tuna1 chop-loc)")
+state = execute(domain, state, pddl"(put-down s-knife1 chop-loc)")
+state = execute(domain, state, pddl"(take-out tuna1 board1 chop-loc)")
 state = execute(domain, state, pddl"(move chop-loc plate-loc)")
-state = execute(domain, state, pddl"(place-in fish1 plate1 plate-loc)")
-satisfy(domain, state, problem.goal)
+state = execute(domain, state, pddl"(place-in tuna1 plate1 plate-loc)")
+@assert satisfy(domain, state, problem.goal)
 
-goal = pddl"
-(exists (?hamburger-bun - food ?beef - food ?cheese - food ?onion - food ?plate - receptacle)
-        (and (food-type beef ?beef)
-             (food-type hamburger-bun ?hamburger-bun)
-             (food-type cheese ?cheese)
-             (food-type onion ?onion)
-             (receptacle-type plate ?plate)
-             (cooked grill ?beef)
-             (prepared slice ?cheese)
-             (prepared chop ?onion)
-             (in-receptacle ?hamburger-bun ?plate)
-             (in-receptacle ?beef ?plate)
-             (in-receptacle ?cheese ?plate)
-             (in-receptacle ?onion ?plate)))
-"
-goal
-
-statics = PDDL.infer_static_fluents(domain)
-goal = PDDL.to_nnf(PDDL.dequantify(goal, domain, state))
-goal = PDDL.simplify_statics(goal, domain, state, statics)
-
-@time heuristic = precomputed(HMax(), domain, state, problem.goal);
-@time hval = heuristic(domain, state, goal)
-planner = AStarPlanner(HAdd(), save_search=true, max_time=300)
-@time sol = planner(domain, state, problem.goal);
-
+# Use heuristic to check reachability
+state = initstate(domain, problem)
+heuristic = HMax()
+hval = heuristic(domain, state, problem.goal)
 
 # Use planner to solve for goal
-state = initstate(domain, problem)
-planner = AStarPlanner(FFHeuristic(), save_search=true)
-@time sol = planner(domain, state, problem.goal)
+planner = AStarPlanner(HAdd(), save_search=true)
+sol = planner(domain, state, problem.goal)
+
+# Check if solution was found
+@assert !(sol isa NullSolution)
 
 # Print solution
-write_pddl.(collect(sol))
+println("== Solution ==")
+for act in write_pddl.(collect(sol))
+    println(act)
+end
+println("Solution Length: $(length(sol))")
 
-# Load domain and problem 4
+## Load domain and problem 3-1
+domain = load_domain(joinpath(@__DIR__, "domain.pddl"));
+problem = load_problem(joinpath(@__DIR__, "problem-3-1.pddl"));
+
+# Manually execute plan and check if goal is satisfied
+state = initstate(domain, problem)
+# Add hamburger bun to plate
+state = execute(domain, state, pddl"(move start-loc food-loc)")
+state = execute(domain, state, pddl"(pick-up hamburger-bun1 food-loc)")
+state = execute(domain, state, pddl"(move food-loc plate-loc)")
+state = execute(domain, state, pddl"(place-in hamburger-bun1 plate1 plate-loc)")
+# Cook and transfer beef to plate
+state = execute(domain, state, pddl"(move plate-loc food-loc)")
+state = execute(domain, state, pddl"(pick-up beef1 food-loc)")
+state = execute(domain, state, pddl"(move food-loc stove-loc)")
+state = execute(domain, state, pddl"(place-in beef1 pan1 stove-loc)")
+state = execute(domain, state, pddl"(cook grill pan1 stove1 stove-loc)")
+state = execute(domain, state, pddl"(pick-up pan1 stove-loc)")
+state = execute(domain, state, pddl"(move stove-loc plate-loc)")
+state = execute(domain, state, pddl"(transfer pan1 plate1 plate-loc)")
+state = execute(domain, state, pddl"(put-down pan1 plate-loc)")
+# Slice cheese
+state = execute(domain, state, pddl"(move plate-loc food-loc)")
+state = execute(domain, state, pddl"(pick-up cheese1 food-loc)")
+state = execute(domain, state, pddl"(move food-loc chop-loc)")
+state = execute(domain, state, pddl"(place-in cheese1 board1 chop-loc)")
+state = execute(domain, state, pddl"(pick-up knife1 chop-loc)")
+state = execute(domain, state, pddl"(prepare slice board1 knife1 cheese1 chop-loc)")
+state = execute(domain, state, pddl"(put-down knife1 chop-loc)")
+# Transfer sliced cheese to plate
+state = execute(domain, state, pddl"(pick-up board1 chop-loc)")
+state = execute(domain, state, pddl"(move chop-loc plate-loc)")
+state = execute(domain, state, pddl"(transfer board1 plate1 plate-loc)")
+state = execute(domain, state, pddl"(put-down board1 plate-loc)")
+@assert satisfy(domain, state, problem.goal)
+
+# Use heuristic to check reachability
+state = initstate(domain, problem)
+heuristic = HMax()
+hval = heuristic(domain, state, problem.goal)
+
+# Use planner to solve for goal
+planner = AStarPlanner(HAdd())
+sol = planner(domain, state, problem.goal);
+
+# Check if solution was found
+@assert !(sol isa NullSolution)
+
+# Print solution
+println("== Solution ==")
+for act in write_pddl.(collect(sol))
+    println(act)
+end
+println("Solution Length: $(length(sol))")
+
+## Load domain and problem 4-1
 domain = load_domain(joinpath(@__DIR__, "domain.pddl"))
 problem = load_problem(joinpath(@__DIR__, "problem-4-1.pddl"))
 
@@ -137,23 +185,30 @@ state = execute(domain, state, pddl"(pick-up tray1 oven-loc)")
 # Transfer to plate
 state = execute(domain, state, pddl"(move oven-loc plate-loc)")
 state = execute(domain, state, pddl"(transfer tray1 plate1)")
-satisfy(domain, state, problem.goal)
+@assert satisfy(domain, state, problem.goal)
 
-# Use planner to solve for goal
+# Use heuristic to check reachability
 state = initstate(domain, problem)
 heuristic = HMax()
 hval = heuristic(domain, state, problem.goal)
-planner = AStarPlanner(HAdd(), save_search=true, max_time=300)
-@time sol = planner(domain, state, problem.goal);
+
+# Use planner to solve for goal
+planner = AStarPlanner(HAdd())
+sol = planner(domain, state, problem.goal);
+
+# Check if solution was found
+@assert !(sol isa NullSolution)
 
 # Print solution
-write_pddl.(collect(sol)) |> display
+println("== Solution ==")
+for act in write_pddl.(collect(sol))
+    println(act)
+end
+println("Solution Length: $(length(sol))")
 
-# Load domain and problem 5
+## Load domain and problem 5-1
 domain = load_domain(joinpath(@__DIR__, "domain.pddl"))
 problem = load_problem(joinpath(@__DIR__, "problem-5-1.pddl"))
-
-available(domain, state)
 
 # Manually execute plan and check if goal is satisfied
 state = initstate(domain, problem)
@@ -170,9 +225,8 @@ state = execute(domain, state, pddl"(move mix-loc food-loc)")
 state = execute(domain, state, pddl"(pick-up chocolate1 food-loc)")
 state = execute(domain, state, pddl"(move food-loc mix-loc)")
 state = execute(domain, state, pddl"(place-in chocolate1 mixing-bowl1 mix-loc)")
-#Mix ingredients
+# Mix ingredients
 state = execute(domain, state, pddl"(combine mix mixing-bowl1 mixer1 mix-loc)")
-
 # Bake cake ingredients
 state = execute(domain, state, pddl"(pick-up mixing-bowl1 mix-loc)")
 state = execute(domain, state, pddl"(move mix-loc oven-loc)")
@@ -182,14 +236,23 @@ state = execute(domain, state, pddl"(pick-up mixing-bowl1 oven-loc)")
 # Transfer to plate
 state = execute(domain, state, pddl"(move oven-loc plate-loc)")
 state = execute(domain, state, pddl"(transfer mixing-bowl1 plate1)")
-satisfy(domain, state, problem.goal)
+@assert satisfy(domain, state, problem.goal)
 
-# Use planner to solve for goal
+# Use heuristic to check reachability
 state = initstate(domain, problem)
 heuristic = HMax()
 hval = heuristic(domain, state, problem.goal)
-planner = AStarPlanner(HAdd(), save_search=true, max_time=300)
-@time sol = planner(domain, state, problem.goal);
+
+# Use planner to solve for goal
+planner = AStarPlanner(HAdd())
+sol = planner(domain, state, problem.goal);
+
+# Check if solution was found
+@assert !(sol isa NullSolution)
 
 # Print solution
-write_pddl.(collect(sol)) |> display
+println("== Solution ==")
+for act in write_pddl.(collect(sol))
+    println(act)
+end
+println("Solution Length: $(length(sol))")
