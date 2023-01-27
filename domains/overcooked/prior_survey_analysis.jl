21c9f131efe207644e4bbb0ad0b77d74e36c0130
Xuan <tanqazx@gmail.com>
2023-01-26 21:08:38 -0500

    Update survey analysis.

--- domains/overcooked/prior_survey_analysis.jl
+++ domains/overcooked/prior_survey_analysis.jl
@@ -1,8 +1,5 @@
 using CSV, DataFrames, Statistics
 
-"Skip NaN values."
-skipnan(x) = Iterators.filter(!isnan, x)
-
 # Names of recipe sources
 RECIPE_SOURCES = [
     "baseline",
@@ -24,6 +21,9 @@ df_path = "prior_survey_results.csv"
 df_path = joinpath(@__DIR__, df_path)
 df = CSV.read(df_path, DataFrame, header=2, skipto=4)
 
+# Filter by comprehension check score
+filter!(row -> row["Score"] >= 2, df)
+
 # Filter column names that store recipe ratings
 rating_cols = filter(names(df)) do name
     match(r"Kitchen-(\d+)-Goal-(\d+) - (\d+) - (\w+)", name) !== nothing
@@ -36,7 +36,8 @@ stacked_df = stack(df, rating_cols)
 # Compute mean and std across participants for each rating
 agg_df = combine(groupby(stacked_df, :variable),
     :value => (x -> mean(skipmissing(x))) => :rating_mean,
-    :value => (x -> std(skipmissing(x))) => :rating_std
+    :value => (x -> std(skipmissing(x))) => :rating_std,
+    :value => (x -> length(collect(skipmissing(x)))) => :n_ratings
 )
 
 # Transform rating field name to multiple fields
@@ -56,25 +57,37 @@ function parse_rating_name(names)
             source=sources, rating_type=rating_types)
 end
 
-df = transform(agg_df, :variable => parse_rating_name => AsTable)
-select!(df, [4:8..., 2, 3]) # Reorder columns
+df = transform(stacked_df, :variable => parse_rating_name => AsTable)
+select!(df, [1, 4:9..., 3]) # Reorder columns
 
 # Compute mean ratings for each source
 source_gdf = groupby(df, [:source, :rating_type])
 source_df = combine(source_gdf,
-    :rating_mean => (x -> x |> skipnan |> mean) => :rating_mean,
-    :rating_mean => (x -> x |> skipnan |> std)  => :rating_std,
-    :rating_mean => (x -> (x |> skipnan |> std) / sqrt(length(collect(skipnan(x))))) => :rating_sem
+    :value => (x -> x |> skipmissing |> mean) => :rating_mean,
+    :value => (x -> x |> skipmissing |> std)  => :rating_std,
+    :value => (x -> (x |> skipmissing |> std) / sqrt(length(collect(skipmissing(x))))) => :rating_sem
 )
+transform!(source_df, :rating_sem => (x -> 1.96 .* x) => :rating_ci)
 source_df_path = joinpath(@__DIR__, "prior_survey_analysis_per_source.csv")
 CSV.write(source_df_path, source_df)
 
 # Compute mean ratings for each source and kitchen type
 kitchen_gdf = groupby(df, [:source, :kitchen_name, :rating_type])
 kitchen_df = combine(kitchen_gdf,
-    :rating_mean => (x -> x |> skipnan |> mean) => :rating_mean,
-    :rating_mean => (x -> x |> skipnan |> std)  => :rating_std,
-    :rating_mean => (x -> (x |> skipnan |> std) / sqrt(length(x))) => :rating_sem
+    :value => (x -> x |> skipmissing |> mean) => :rating_mean,
+    :value => (x -> x |> skipmissing |> std)  => :rating_std,
+    :value => (x -> (x |> skipmissing |> std) / sqrt(length(collect(skipmissing(x))))) => :rating_sem
 )
+transform!(kitchen_df, :rating_sem => (x -> 1.96 .* x) => :rating_ci)
 kitchen_df_path = joinpath(@__DIR__, "prior_survey_analysis_per_kitchen.csv")
 CSV.write(kitchen_df_path, kitchen_df)
+
+# Compute mean ratings for each source and problem
+problem_gdf = groupby(df, [:source, :kitchen_name, :problem_id, :rating_type])
+problem_df = combine(problem_gdf,
+    :value => (x -> x |> skipmissing |> mean) => :rating_mean,
+    :value => (x -> x |> skipmissing |> std)  => :rating_std,
+    :value => (x -> (x |> skipmissing |> std) / sqrt(length(collect(skipmissing(x))))) => :rating_sem
+)
+problem_df_path = joinpath(@__DIR__, "prior_survey_analysis_per_problem.csv")
+CSV.write(problem_df_path, problem_df)
