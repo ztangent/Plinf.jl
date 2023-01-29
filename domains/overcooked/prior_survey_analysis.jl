8abd6705df2a668e53a1fa4f9eb2858a31ec73c7
Xuan <tanqazx@gmail.com>
2023-01-29 18:31:24 -0500

    Add plotting code for analyis scripts.

--- domains/overcooked/prior_survey_analysis.jl
+++ domains/overcooked/prior_survey_analysis.jl
@@ -89,5 +89,101 @@ problem_df = combine(problem_gdf,
     :value => (x -> x |> skipmissing |> std)  => :rating_std,
     :value => (x -> (x |> skipmissing |> std) / sqrt(length(collect(skipmissing(x))))) => :rating_sem
 )
+transform!(problem_df, :rating_sem => (x -> 1.96 .* x) => :rating_ci)
 problem_df_path = joinpath(@__DIR__, "prior_survey_analysis_per_problem.csv")
 CSV.write(problem_df_path, problem_df)
+
+## Plotting ##
+using StatsPlots, CategoricalArrays
+
+default(fontfamily="Arial", palette=:Egypt, tickfontsize=12)
+
+# Plot per-source results
+plot_df = unstack(source_df, [:rating_type], :source, :rating_mean)
+plot_err_df = unstack(source_df, [:rating_type], :source, :rating_ci)
+
+# Ensure categories preserve order instead of following alphabetical order
+group_labels = plot_df.rating_type
+groups = CategoricalArray(repeat(group_labels, outer=3))
+levels!(groups, group_labels)
+
+source_labels = ["Baseline Prior", "InstructGPT", "Human Crafted"]
+categories = CategoricalArray(repeat(source_labels, inner=3))
+levels!(categories, source_labels)
+
+# Construct grouped bar plot
+ratings = Matrix(plot_df[:, 2:end])
+errs = Matrix(plot_err_df[:, 2:end])
+groupedbar(groups, ratings, errorbar=errs,
+           group=categories, ylabel="Human Rating",
+           ylims=(0, 7), size=(640, 360), dpi=300)
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_source.pdf"))
+title!("Human Ratings of \n Human vs. Machine-Generated Recipes\n")
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_source.png"))
+
+# Plot per-kitchen results
+subplots = []
+for (key, sub_df) in pairs(groupby(kitchen_df, :kitchen_name))
+    plot_df = unstack(sub_df, [:rating_type], :source, :rating_mean)
+    plot_err_df = unstack(sub_df, [:rating_type], :source, :rating_ci)
+
+    # Ensure categories preserve order instead of following alphabetical order
+    group_labels = plot_df.rating_type
+    groups = CategoricalArray(repeat(group_labels, outer=3))
+    levels!(groups, group_labels)
+
+    source_labels = ["Baseline Prior", "InstructGPT", "Human Crafted"]
+    categories = CategoricalArray(repeat(source_labels, inner=3))
+    levels!(categories, source_labels)
+
+    # Construct grouped bar plot
+    ratings = Matrix(plot_df[:, 2:end])
+    errs = Matrix(plot_err_df[:, 2:end])
+    p = groupedbar(groups, ratings, errorbar=errs, legend=false,
+                   group=categories, ylims=(0, 7),
+                   title="\n" * titlecase(key.kitchen_name))
+    push!(subplots, p)
+end
+ylabel!(subplots[1], "Human Rating")
+plot!(subplots[1], left_margin = 40 * Plots.px)
+plot!(subplots[end], legend=true, legend_font_pointsize=5)
+
+plot(subplots..., layout=(1, 5), size=(1600, 400), dpi=300,
+     top_margin = 40 .* Plots.px, bottom_margin = 20 .* Plots.px)
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_kitchen.pdf"))     
+plot!(plot_title="Human Ratings of Human vs. Machine-Generated Recipes (per sub-domain)")
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_kitchen.png"))
+
+# Plot per-problem results
+subplots = []
+for (i, (key, sub_df)) in enumerate(pairs(groupby(problem_df, [:kitchen_name, :problem_id])))
+    plot_df = unstack(sub_df, [:rating_type], :source, :rating_mean)
+    plot_err_df = unstack(sub_df, [:rating_type], :source, :rating_ci)
+
+    # Ensure categories preserve order instead of following alphabetical order
+    group_labels = plot_df.rating_type
+    groups = CategoricalArray(repeat(group_labels, outer=3))
+    levels!(groups, group_labels)
+
+    source_labels = ["Baseline Prior", "InstructGPT", "Human Crafted"]
+    categories = CategoricalArray(repeat(source_labels, inner=3))
+    levels!(categories, source_labels)
+
+    # Construct grouped bar plot
+    ratings = Matrix(plot_df[:, 2:end])
+    errs = Matrix(plot_err_df[:, 2:end])
+    title_str = "$(titlecase(key.kitchen_name)) $(key.problem_id)"
+    # if i <= 5 # Pad title with newline for first row
+    #     title_str = "\n" * title_str
+    # end
+    p = groupedbar(groups, ratings, errorbar=errs, legend=false,
+                   group=categories, ylims=(0, 7.25), yticks=1:7,
+                   title=title_str)
+    push!(subplots, p)
+end
+
+plot(subplots..., layout=(5, 5), size=(1600, 1600), dpi=300)
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_problem.pdf"))
+plot(subplots..., layout=(5, 5), size=(1600, 1700), dpi=300)
+plot!(plot_title="Human Ratings of Human vs. Machine-Generated Recipes \n (per environment)")
+savefig(joinpath(@__DIR__, "goal_generation_human_results_per_problem.png"))
--- domains/overcooked/recipe_analysis_gpt3_eng.jl
+++ domains/overcooked/recipe_analysis_gpt3_eng.jl
@@ -24,12 +24,30 @@ df = DataFrame(
 df_types = eltype.(eachcol(df))
 
 # Define columns that correspond to experimental conditions
-condition_cols = [:kitchen_name, :temperature, :n_train_recipes_per_kitchen, :model, :include_recipe_description]
+condition_cols = [:model, :include_recipe_description]
 
 # Load dataframe
-df_path = "recipes_gpt3_eng_text-davinci-003_temp_1.0_nperkitchen_3_2023-01-22T14-31-25.csv"
+df_path = "recipes_gpt3_eng_text-davinci-003_temp_1.0_nperkitchen_3_2023-01-23T17-52-40.csv"
 df_path = joinpath(@__DIR__, df_path)
 df = CSV.read(df_path, DataFrame, types=df_types)
+
+df_path = "recipes_gpt3_eng_text-davinci-003_temp_1.0_nperkitchen_3_2023-01-27T21-03-51.csv"
+df_path = joinpath(@__DIR__, df_path)
+append!(df, CSV.read(df_path, DataFrame, types=df_types))
+
+df_path = "recipes_gpt3_eng_davinci_temp_1.0_nperkitchen_3_2023-01-23T21-15-58.csv"
+df_path = joinpath(@__DIR__, df_path)
+append!(df, CSV.read(df_path, DataFrame, types=df_types))
+
+df_path = "recipes_gpt3_eng_davinci_temp_1.0_nperkitchen_3_2023-01-27T22-59-36.csv"
+df_path = joinpath(@__DIR__, df_path)
+append!(df, CSV.read(df_path, DataFrame, types=df_types))
+
+df_path = "recipes_baseline_2023-01-22T13-26-42.csv"
+df_path = joinpath(@__DIR__, df_path)
+baseline_df = CSV.read(df_path, DataFrame)
+baseline_df.model .= "baseline"
+append!(df, baseline_df; cols=:subset)
 
 # Compute various extra information
 transform!(df, :reason => (x -> x .== "Parse error") => :parse_error)
@@ -38,20 +56,29 @@ transform!(df, :reason => (x -> x .== "Non-existent objects or variables") => :n
 transform!(df, :reason => (x -> x .== "Goal is not reachable") => :goal_unreachable)
 
 # Group data frame by problems and conditions
-problem_gdf = groupby(df, [condition_cols; :problem])
+problem_gdf = groupby(df, [condition_cols; [:problem, :kitchen_name]])
+kitchen_gdf = groupby(df, [condition_cols; :kitchen_name])
 condition_gdf = groupby(df, condition_cols)
 
 ## Validity Metrics ##
 
 # Define operations and columns
+ci(xs) = 1.96 * sem(xs) # 95% confidence interval
+# validity_cols =
+#     [:valid, :parse_error,:non_existent_objects, :goal_unreachable]
 validity_cols =
     [:valid, :parse_error,
      :non_existent_predicates, :non_existent_objects, :goal_unreachable]
-validity_sem_ops = reduce(vcat, ([col => mean, col => sem] for col in validity_cols))
-validity_ops = [col => mean for col in validity_cols]
+mean_ops = [col => mean for col in validity_cols]
+sem_ops = [col => sem for col in validity_cols]
+ci_ops = [col => ci for col in validity_cols]
+validity_ops = [mean_ops; sem_ops; ci_ops]
      
 #  Compute validity metrics for each problem and condition
 prob_validity_df = combine(problem_gdf, validity_ops...)
+
+#  Compute validity metrics for each kitchen and condition
+kitchen_validity_df = combine(kitchen_gdf, validity_ops...)
 
 #  Compute average validity metrics for each condition
 mean_validity_df = combine(condition_gdf, validity_ops...)
@@ -90,6 +117,10 @@ prob_diversity_df = combine(problem_gdf,
     [:valid, :logprobs] => entropy_of_valid_goals => :entropy_valid
 )
 
+# Average diversity metrics over problems within each kitchen
+diversity_gdf = groupby(prob_diversity_df, [condition_cols; :kitchen_name])
+kitchen_diversity_df = combine(diversity_gdf, [col => mean => col for col in diversity_cols])
+
 # Average diversity metrics over problems within each condition
 diversity_gdf = groupby(prob_diversity_df, condition_cols)
 mean_diversity_df = combine(diversity_gdf, [col => mean => col for col in diversity_cols])
@@ -97,15 +128,101 @@ mean_diversity_df = combine(diversity_gdf, [col => mean => col for col in divers
 ## Merge Results ##
 
 # Merge validity and diversity results for each problem
-prob_df = innerjoin(prob_validity_df, prob_diversity_df, on=[condition_cols; :problem])
+prob_df = innerjoin(prob_validity_df, prob_diversity_df, matchmissing=:equal,
+                    on=[condition_cols; [:problem, :kitchen_name]])
+transform!(prob_df, [:frac_unique_valid, :valid_mean] => ((x, y) -> x./y) => :frac_unique_out_of_valid)
+
+# Merge validity and diversity results for each kitchen
+kitchen_df = innerjoin(kitchen_validity_df, kitchen_diversity_df, matchmissing=:equal,
+                       on=[condition_cols; :kitchen_name])
 transform!(prob_df, [:frac_unique_valid, :valid_mean] => ((x, y) -> x./y) => :frac_unique_out_of_valid)
 
 # Merge validity and diversity results for each condition
-mean_df = innerjoin(mean_validity_df, mean_diversity_df, on=condition_cols)
+mean_df = innerjoin(mean_validity_df, mean_diversity_df,
+                    matchmissing=:equal, on=condition_cols)
 transform!(mean_df, [:frac_unique_valid, :valid_mean] => ((x, y) -> x./y) => :frac_unique_out_of_valid)
 
 # Write out files
-prob_df_path = joinpath(@__DIR__, "analysis_per_problem_gpt3_eng.csv")
+prob_df_path = joinpath(@__DIR__, "analysis_per_problem.csv")
 CSV.write(prob_df_path, prob_df)
-mean_df_path = joinpath(@__DIR__, "analysis_per_condition_gpt3_eng.csv")
+kitchen_df_path = joinpath(@__DIR__, "analysis_per_kitchen.csv")
+CSV.write(kitchen_df_path, kitchen_df)
+mean_df_path = joinpath(@__DIR__, "analysis_per_condition.csv")
 CSV.write(mean_df_path, mean_df)
+
+## Plotting ##
+using StatsPlots, CategoricalArrays
+using ColorSchemes
+
+palette = ColorSchemes.Hiroshige[[5,3,1,7]]
+default(fontfamily="Arial", palette=palette, tickfontsize=12)
+
+validity_mean_cols = [Symbol(name, "_mean") for name in validity_cols
+                      if name != :non_existent_predicates]
+validity_ci_cols = [Symbol(name, "_ci") for name in validity_cols
+                    if name != :non_existent_predicates]
+
+# Plot per-model results
+plot_df = select(mean_df, Not(r"non_existent_predicates"))
+
+# Ensure categories preserve order instead of following alphabetical order
+group_labels = ["InstructGPT\n(No Desc.)", "InstructGPT\n(Desc.)",
+                "GPT-3\n(No Desc.)", "GPT-3\n(Desc.)", "Baseline\nPrior"]
+groups = CategoricalArray(repeat(group_labels, outer=4))
+levels!(groups, group_labels)
+
+# category_labels = reverse!(["Valid", "Parse Error", "Spurious Predicates", "Spurious Objects", "Unachievable Goal"])
+category_labels = reverse!(["Valid", "Parse Error", "Spurious Objects", "Unachievable Goal"])
+categories = CategoricalArray(repeat(category_labels, inner=5))
+levels!(categories, category_labels)
+
+# Construct grouped bar plot
+validity = Matrix(plot_df[:, reverse(validity_mean_cols)])
+errs = Matrix(plot_df[:, reverse(validity_ci_cols)])
+groupedbar(groups, validity, errorbar=errs, bar_position=:stack,
+           group=categories, ylabel="Fraction", legendposition=:bottomleft,
+           ylims=(0, 1.1), size=(640, 360), dpi=300)
+savefig(joinpath(@__DIR__, "goal_generation_computational_results_per_model.pdf"))
+title!("Validity of Generated Recipes across Different Models")
+plot!(legendposition=:outerright, size=(900, 360),
+      bottom_margin=20*Plots.px, left_margin=20*Plots.px)
+savefig(joinpath(@__DIR__, "goal_generation_computational_results_per_model.png"))
+
+# Plot per-kitchen results
+plot_df = select(kitchen_df, Not(r"non_existent_predicates"))
+
+plot_titles = ["InstructGPT (No Description)", "InstructGPT (Description)",
+               "GPT-3 (No Description)", "GPT-3 (Description)", "Baseline Prior"]
+category_labels = reverse!(["Valid", "Parse Error", "Spurious Objects", "Unachievable Goal"])
+
+subplots = []
+for (i, (key, sub_df)) in enumerate(pairs(groupby(plot_df, [:model, :include_recipe_description])))
+    # Ensure categories preserve order instead of following alphabetical order
+    group_labels = titlecase.(replace.(sub_df.kitchen_name, "and" => "&"))
+    groups = CategoricalArray(repeat(group_labels, outer=5))
+    levels!(groups, group_labels)
+
+    categories = CategoricalArray(repeat(category_labels, inner=5))
+    levels!(categories, category_labels)
+
+    # Construct grouped bar plot
+    validity = Matrix(sub_df[:, reverse(validity_mean_cols)])
+    errs = Matrix(sub_df[:, reverse(validity_ci_cols)])
+    p = groupedbar(groups, validity, errorbar=errs, legend=false,
+                   group=categories, ylims=(0, 1.1), bar_position=:stack,
+                   title="\n" * plot_titles[i], xrotation=-20.0,
+                   xtickfontsize=10, xtickfontvalign=:bottom)
+    push!(subplots, p)
+end
+legend_plot = plot(
+    fill(missing, (1, 4)), seriestype=:bar, linewidth=1, framestyle=:none,
+         legend=true, labels=permutedims(category_labels),
+         legendposition=:left, legend_font_pointsize=10,
+         left_margin=-20 * Plots.px
+    )
+l = @layout [grid(1, 5) lp{0.1w}]
+plot(subplots..., legend_plot, layout=l, size=(2000, 400), dpi=300,
+     top_margin = 40 .* Plots.px, bottom_margin = 50 .* Plots.px)
+savefig(joinpath(@__DIR__, "goal_generation_computational_results_per_kitchen.pdf"))
+plot!(plot_title="Validity of Generated Recipes across Different Models and Subdomains")
+savefig(joinpath(@__DIR__, "goal_generation_computational_results_per_kitchen.png"))
