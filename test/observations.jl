a79b35c203555318ece2323a4f4b6a5621e7c3e5
Xuan <tanqazx@gmail.com>
2023-02-01 02:49:06 -0500

    Fix rest of observation tests.

--- test/observations.jl
+++ test/observations.jl
@@ -19,7 +19,7 @@ bw_state = initstate(blocksworld, bw_problem)
 @testset "Observed Facts" begin
 
 # Test lifted observation model
-bw_params = observe_params(
+bw_params = ObsNoiseParams(
     (pddl"(forall (?x - block ?y - block) (on ?x ?y))", 0.05),
     (pddl"(forall (?x - block) (clear ?x))", 0.05),
     (pddl"(forall (?x - block) (ontable ?x))", 0.05),
@@ -27,8 +27,7 @@ bw_params = observe_params(
 )
 bw_terms = collect(keys(bw_params))
 
-obs_choices =
-    state_choicemap(bw_state, bw_terms; addr=nothing, domain=blocksworld)
+obs_choices = state_choicemap(bw_state, bw_terms; domain=blocksworld)
 @test obs_choices[pddl"(handempty)"] == true
 @test obs_choices[pddl"(ontable a)"] == true
 @test obs_choices[pddl"(clear b)"] == true
@@ -39,7 +38,7 @@ obs_choices =
 bw_params = ground_obs_params(bw_params, blocksworld, bw_state)
 bw_terms = collect(keys(bw_params))
 
-obs_choices = state_choicemap(bw_state, blocksworld, bw_terms, nothing)
+obs_choices = state_choicemap(bw_state, bw_terms)
 @test obs_choices[pddl"(handempty)"] == true
 @test obs_choices[pddl"(ontable a)"] == true
 @test obs_choices[pddl"(clear b)"] == true
@@ -47,10 +46,10 @@ obs_choices = state_choicemap(bw_state, blocksworld, bw_terms, nothing)
 @test obs_choices[pddl"(on a b)"] == false
 
 # Test automatically generated observation model
-bw_params = observe_params(blocksworld, state=bw_state, pred_noise=0.05)
+bw_params = ObsNoiseParams(blocksworld, bw_state, pred_noise=0.05)
 bw_terms = collect(keys(bw_params))
 
-obs_choices = state_choicemap(bw_state, blocksworld, bw_terms, nothing)
+obs_choices = state_choicemap(bw_state, bw_terms)
 @test obs_choices[pddl"(handempty)"] == true
 @test obs_choices[pddl"(ontable a)"] == true
 @test obs_choices[pddl"(clear b)"] == true
@@ -60,7 +59,7 @@ obs_choices = state_choicemap(bw_state, blocksworld, bw_terms, nothing)
 # Test constrained observations
 obs_choices[pddl"(handempty)"] = false
 trace, weight =
-    generate(observe_state, (bw_state, blocksworld, bw_params), obs_choices)
+    generate(observe_state, (blocksworld, bw_state, bw_params), obs_choices)
 obs_state = get_retval(trace)
 expected_weight = log(0.05) + (length(bw_terms) - 1) * log(1 - 0.05)
 
@@ -73,19 +72,19 @@ end
 @testset "Observed Fluents" begin
 
 # Test fluent observation model
-gw_params = observe_params(
+gw_params = ObsNoiseParams(
     (pddl"(xpos)", normal, 0.25), (pddl"(ypos)", normal, 0.25)
 )
 gw_terms = collect(keys(gw_params))
 
-obs_choices = state_choicemap(gw_state, gridworld, gw_terms, nothing)
+obs_choices = state_choicemap(gw_state, gw_terms)
 @test obs_choices[pddl"(xpos)"] == 1
-@test obs_choices[pddl"(ypos)"] == 3
+@test obs_choices[pddl"(ypos)"] == 1
 
 # Test constrained observations
 obs_choices[pddl"(ypos)"] = 2
 trace, weight =
-    generate(observe_state, (gw_state, gridworld, gw_params), obs_choices)
+    generate(observe_state, (gridworld, gw_state, gw_params), obs_choices)
 obs_state = get_retval(trace)
 expected_weight = logpdf(normal, 1, 1, 0.25) + logpdf(normal, 2, 3, 0.25)
 
