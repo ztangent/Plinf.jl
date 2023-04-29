99f9cc4b54a01ef66dd7336644b7d3c8f174d378
Xuan <tanqazx@gmail.com>
2023-04-29 15:37:33 -0400

    Bugfixes to goal rejuvenation kernels.

--- src/inference/rejuvenate.jl
+++ src/inference/rejuvenate.jl
@@ -1,6 +1,7 @@
 export RejuvenationKernel
 export NullKernel, SequentialKernel, MixtureKernel
-export ReplanKernel, InitGoalKernel, RecentGoalKernel
+export ReplanKernel, ConsecutiveReplanKernel
+export InitGoalKernel, RecentGoalKernel, ConsecutiveGoalKernel
 
 "Abstract type for MCMC rejuvenation kernels."
 abstract type RejuvenationKernel end
@@ -82,6 +83,23 @@ function (kernel::ReplanKernel)(trace::Trace)
 end
 
 """
+    ConsecutiveReplanKernel(n::Int, order::Symbol=:forward)
+
+Performs `n` consecutive Metropolis-Hastings resimulation moves on the agent's
+planning steps. The `order` argument determines whether the moves are performed
+in `:forward` (earliest first) or `:backward` order (most recent first).
+"""
+function ConsecutiveReplanKernel(n::Int, order::Symbol=:forward)
+    @assert order in (:forward, :backward)
+    if order == :forward
+        subkernels = ntuple(i -> ReplanKernel(n-i+1), n)
+    else
+        subkernels = ntuple(i -> ReplanKernel(i), n)
+    end
+    return SequentialKernel(subkernels)
+end
+
+"""
     InitGoalKernel()
 
 Perform a Metropolis-Hastings resimulation move on the agent's initial goal.
@@ -89,14 +107,19 @@ Perform a Metropolis-Hastings resimulation move on the agent's initial goal.
 struct InitGoalKernel <: RejuvenationKernel end
 
 function (kernel::InitGoalKernel)(trace::Trace)
-    return mh(trace, select(:init => :agent => :goal))
+    # Rejuvenate goal and all downstream agent choices
+    n_steps = Gen.get_args(trace)[1]
+    goal_addrs = (:timestep => t => :agent => :goal for t in 1:n_steps)
+    plan_addrs = (:timestep => t => :agent => :plan for t in 1:n_steps)
+    sel = select(:init => :agent => :goal, goal_addrs..., plan_addrs...)
+    return mh(trace, sel)
 end
 
 """
     RecentGoalKernel(n::Int=1)
 
 Performs Metropolis-Hastings resimulation moves on the agent's goals for the
-past `n` steps, starting from the earliest of those steps.
+past `n` steps.
 """
 struct RecentGoalKernel <: RejuvenationKernel
     n::Int
@@ -105,11 +128,27 @@ end
 RecentGoalKernel() = RecentGoalKernel(1)
 
 function (kernel::RecentGoalKernel)(trace::Trace)
-    accept = false
     n_steps = Gen.get_args(trace)[1]
     start = max(n_steps-kernel.n+1, 1)
-    for t in start:n_steps
-        trace, accept = mh(trace, select(:timestep => t => :agent => :goal))
+    goal_addrs = (:timestep => t => :agent => :goal for t in start:n_steps)
+    plan_addrs = (:timestep => t => :agent => :plan for t in start:n_steps)
+    sel = select(goal_addrs..., plan_addrs...)
+    return mh(trace, sel)
+end
+
+"""
+    ConsecutiveGoalKernel(n::Int, order::Symbol=:forward)
+
+Performs `n` consecutive Metropolis-Hastings resimulation moves on the agent's
+goals. The `order` argument determines whether the moves are performed in
+`:forward` (earliest first) or `:backward` order (most recent first).
+"""
+function ConsecutiveGoalKernel(n::Int, order::Symbol=:forward)
+    @assert order in (:forward, :backward)
+    if order == :forward
+        subkernels = ntuple(i -> RecentGoalKernel(n-i+1), n)
+    else
+        subkernels = ntuple(i -> RecentGoalKernel(i), n)
     end
-    return trace, accept
+    return SequentialKernel(subkernels)
 end
