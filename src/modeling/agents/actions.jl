0b97dade544b8b8e3f724e962fb03708dee4395a
Xuan <tanqazx@gmail.com>
2023-06-07 11:49:38 -0400

    Support act initialization, communication history.

--- src/modeling/agents/actions.jl
+++ src/modeling/agents/actions.jl
@@ -13,8 +13,12 @@ Configuration of an agent's action model.
 
 $(FIELDS)
 """
-struct ActConfig{V}
-    "Transition function with arguments `(t, agent_state, env_state, step_args...)`."
+struct ActConfig{T,U,V}
+    "Initializer with arguments `(agent_state, env_state, init_args...)`."
+    init::T
+    "Trailing arguments to initializer."
+    init_args::U
+    "Transition function with arguments `(t, act_state, agent_state, env_state, step_args...)`."
     step::GenerativeFunction
     "Trailing arguments to transition function."
     step_args::V
@@ -29,16 +33,16 @@ Constructs an `ActConfig` which deterministically selects the planned best
 action for the current state, give the current plan or policy.
 """
 function DetermActConfig()
-    return ActConfig(determ_act_step, ())
+    return ActConfig(PDDL.no_op, (), determ_act_step, ())
 end
 
 """
-    determ_act_step(t, agent_state, env_state)
+    determ_act_step(t, act_state, agent_state, env_state)
 
 Deterministic action selection from the current plan or policy. Returns
 a no-op action if the goal has been achieved.
 """
-@gen function determ_act_step(t, agent_state, env_state)
+@gen function determ_act_step(t, act_state, agent_state, env_state)
     # Planning solution is assumed to be a deterministic plan or policy
     plan_state = agent_state.plan_state::PlanState
     act = {:act} ~ policy_dist(plan_state.sol, env_state)
@@ -54,16 +58,16 @@ Constructs an `ActConfig` which selects a random action `epsilon` of the time
 and otherwise selects the best/planned action.
 """
 function EpsilonGreedyActConfig(domain::Domain, epsilon::Real)
-    return ActConfig(eps_greedy_act_step, (domain, epsilon))
+    return ActConfig(PDDL.no_op, (), eps_greedy_act_step, (domain, epsilon))
 end
 
 """
-    eps_greedy_act_step(t, agent_state, env_state, domain, epsilon)
+    eps_greedy_act_step(t, act_state, agent_state, env_state, domain, epsilon)
 
 Samples an available action uniformly at random `epsilon` of the time, otherwise
 selects the best action.
 """
-@gen function eps_greedy_act_step(t, agent_state, env_state,
+@gen function eps_greedy_act_step(t, act_state, agent_state, env_state,
                                   domain::Domain, epsilon::Real)
     plan_state = agent_state.plan_state::PlanState
     policy = EpsilonGreedyPolicy(domain, plan_state.sol, epsilon)
@@ -80,16 +84,17 @@ Constructs an `ActConfig` which samples actions according to the Boltzmann
 distribution over action Q-values with a specified `temperature`.
 """
 function BoltzmannActConfig(temperature::Real)
-    return ActConfig(boltzmann_act_step, (temperature,))
+    return ActConfig(PDDL.no_op, (), boltzmann_act_step, (temperature,))
 end
 
 """
-    boltzmann_act_step(t, agent_state, env_state, temperature)
+    boltzmann_act_step(t, act_state, agent_state, env_state, temperature)
 
 Samples actions according to the Boltzmann distribution over action values with
 a specified `temperature`
 """
-@gen function boltzmann_act_step(t, agent_state, env_state, temperature::Real)
+@gen function boltzmann_act_step(t, act_state, agent_state, env_state,
+                                 temperature::Real)
     plan_state = agent_state.plan_state::PlanState
     policy = BoltzmannPolicy(plan_state.sol, temperature)
     act = {:act} ~ policy_dist(policy, env_state)
@@ -101,12 +106,13 @@ end
 """
     CommunicativeActState
 
-State of a communicative action model, containing an `action` term and
-`utterance` string.
+State of a communicative action model, containing an `action` term, `utterance`
+string, and communication `history`.
 """
-struct CommunicativeActState
+struct CommunicativeActState{T}
     action::Term
     utterance::String
+    history::T
 end
 
 Base.convert(::Type{Term}, state::CommunicativeActState) = state.action
@@ -119,32 +125,77 @@ Base.convert(::Type{Term}, state::CommunicativeActState) = state.action
     )
 
 Constructs an `ActConfig` which samples an action and utterance jointly,
-combined of an existing (non-communicative) `act_config` and an utterance model
-with arguments `(t, agent_state, env_state, act, utterance_args...)`.
+combining a (non-communicative) `act_config` with an `utterance_model`. At each
+step, a `new_act` is sampled according to `act_config.step`:
+
+    new_act ~ act_config.step(t, act_state, agent_state, env_state,
+                              act_step_args...)
+
+Then an `utterance` is sampled from the `utterance_model` given `new_act`:
+
+    utterance ~ utterance_model(t, act_state, agent_state, env_state,
+                                new_act, utterance_args...)
+
+The `utterance_model` may return an `utterance` as a string, or a
+(`utterance`, `history`) tuple. The communication `history` is passed to the
+next step in a `CommunicativeActState`, but defaults to `nothing` if not
+returned by the `utterance_model`.
 """
 function CommunicativeActConfig(
     act_config::ActConfig, utterance_model::GenerativeFunction,
     utterance_args::Tuple = ()
 )
+    act_init = act_config.init
+    act_init_args = act_config.init_args
     act_step = act_config.step
     act_step_args = act_config.step_args
     return ActConfig(
+        communicative_act_init,
+        (act_init, act_init_args, utterance_model, utterance_args),
         communicative_act_step,
         (act_step, act_step_args, utterance_model, utterance_args)
     )
 end
 
+@gen function communicative_act_init(
+    agent_state, env_state,
+    act_init, act_init_args,
+    utterance_model, utterance_args
+)
+    # Sample action
+    new_act = {*} ~ maybe_sample(act_init, (agent_state, env_state,
+                                            act_init_args...))
+    # Sample utterance
+    init_act_state = CommunicativeActState(PDDL.no_op, "", nothing)
+    result = {*} ~ utterance_model(0, init_act_state, agent_state, env_state,
+                                   new_act, utterance_args...)
+    if isa(result, Tuple)
+        utterance, history = result
+        return CommunicativeActState(act, utterance, history)
+    else
+        utterance = result
+        return CommunicativeActState(act, utterance, nothing)
+    end
+end
+
 @gen function communicative_act_step(
-    t, agent_state, env_state,
+    t, act_state, agent_state, env_state,
     act_step, act_step_args,
     utterance_model, utterance_args
 )
     # Sample action
-    act = {*} ~ act_step(t, agent_state, env_state, act_step_args...)
+    new_act = {*} ~ act_step(t, act_state, agent_state, env_state,
+                             act_step_args...)
     # Sample utterance
-    utterance = {*} ~ utterance_model(t, agent_state, env_state, act,
-                                      utterance_args...)
-    return CommunicativeActState(act, utterance)
+    result = {*} ~ utterance_model(t, act_state, agent_state, env_state,
+                                   new_act, utterance_args...)
+    if isa(result, Tuple)
+        utterance, history = result
+        return CommunicativeActState(act, utterance, history)
+    else
+        utterance = result
+        return CommunicativeActState(act, utterance, nothing)
+    end
 end
 
 # Policy distribution #
@@ -155,22 +206,28 @@ struct PolicyDistribution <: Gen.Distribution{Term} end
     policy_dist(policy, state)
 
 Gen `Distribution` that samples an action from a SymbolicPlanners `policy`
-given the current `state`.
+given the current `state`. If no actions are available, returns `PDDL.no_op`
+with probability 1.0.
 """
 const policy_dist = PolicyDistribution()
 
 (d::PolicyDistribution)(args...) = Gen.random(d, args...)
 
-@inline Gen.random(::PolicyDistribution, policy, state) =
-    SymbolicPlanners.rand_action(policy, state)
-@inline Gen.logpdf(::PolicyDistribution, act::Term, policy, state) =
-    log(SymbolicPlanners.get_action_prob(policy, state, act))
+@inline function Gen.random(::PolicyDistribution, policy, state)
+    act = SymbolicPlanners.rand_action(policy, state)
+    return ismissing(act) ? PDDL.no_op : act
+end
+
+@inline function Gen.logpdf(::PolicyDistribution, act::Term, policy, state)
+    return act.name == PDDL.no_op.name ?
+        0.0 : log(SymbolicPlanners.get_action_prob(policy, state, act))
+end
 
 # Always return no-op for null solutions
 @inline Gen.random(::PolicyDistribution, ::NullSolution, state) =
-    convert(Term, PDDL.no_op)
+    PDDL.no_op
 @inline Gen.logpdf(::PolicyDistribution, act::Term, ::NullSolution, state) =
-    act.name == PDDL.get_name(PDDL.no_op) ? 0.0 : -Inf
+    act.name == PDDL.no_op.name ? 0.0 : -Inf
 
 Gen.logpdf_grad(::PolicyDistribution, act::Term, policy, state) =
     (nothing, nothing, nothing)
